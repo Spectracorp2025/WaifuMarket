582ffe3dd7c01cf115ee7f72f292404d8e59a1f3
Spectracorp2025 <carlosdelgado.neska@gmail.com>
2025-10-02 17:43:33 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,7 +1,14 @@
+// Botón Inicio
 document.getElementById("btn-inicio").addEventListener("click", () => {
   window.location.href = "inicio/index.html";
 });
 
+// Botón Grupos
+document.getElementById("btn-grupos").addEventListener("click", () => {
+  window.location.href = "grupos/index.html";
+});
+
+// Botón Soporte
 document.getElementById("btn-soporte").addEventListener("click", () => {
   window.location.href = "soporte/index.html";
 });
